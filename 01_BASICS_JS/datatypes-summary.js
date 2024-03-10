259e3a4fcbbbcc63a050d1fcf46456f95abf0cbc
Zeel Patel <104027755+7zedd7@users.noreply.github.com>
2024-03-10 09:16:24 +0000

    added Stack and Heap

--- 01_BASICS_JS/datatypes-summary.js
+++ 01_BASICS_JS/datatypes-summary.js
@@ -62,3 +62,47 @@ JavaScript data types and their corresponding return types
     https://262.ecma-international.org/5.1/#sec-11.4.3
 
 */ 
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+/*  Static (Primitive)
+    
+    Primitive data types are stored on the stack, which is a 
+    region of memory that is used to store function calls and 
+    local variables.
+
+*/
+
+/*  Heap (Non-primitive)
+
+    non-primitive data types are stored in the heap. The heap is
+    a memory space that is used to store objects and arrays.
+
+*/
+
+let myName = "Zeel patel"
+
+let anotherName = myName
+anotherName = "Zedd" // When you assign a primitive variable to another, a copy of the value is made.
+
+console.log(myName)
+console.log(anotherName)
+
+let userOne = {
+    email: "abc@gmail.com",
+    upi: "abc@paytm"
+}
+
+let userTwo = userOne
+
+userTwo.email = "xyz@gmail.com" 
+/*
+    When you assign an object or array to another variable,
+    the reference to the same object is copied, not a new copy 
+    of the object. This means that if you make a change to one
+    of the variables, the other variable will also be affected.
+*/
+
+console.log(userOne);
+console.log(userTwo);
