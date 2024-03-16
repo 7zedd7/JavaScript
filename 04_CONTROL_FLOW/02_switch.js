d060caa86e963b08f19221e0e398ff867d32c34d
Zeel Patel <patelzeel7255@gmail.com>
2024-03-16 07:41:42 +0530

    added Switch Case

---
+++ 04_CONTROL_FLOW/02_switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "January":
+        console.log("January");
+        break;
+    case "February":
+        console.log("February");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    
+    case "April":
+        console.log("April");
+        break;
+    default:
+        console.log("Default case Match");
+        break;
+}
+
